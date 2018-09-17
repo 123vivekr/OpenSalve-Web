d0a9a1b00cf1b9d0ca8eeb644f5645a594361d30
123vivekr <123vivekr@gmail.com>
2018-09-17 20:33:52 +0530

    navbar: split navbar into new component

--- src/components/auth.js
+++ src/components/auth.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import NavBar from './navbar';
 
 const styles = theme => ({
         Auth: {
@@ -25,7 +26,7 @@ class Auth extends Component {
             <div className="Auth">
                 <Grid container spacing={24}>
                     <Grid item xs={12}>
-                        <Paper className={classes.paper}>Navbar</Paper>
+                        <NavBar />
                     </Grid>
                     <Grid item xs={12} sm={2}>
                     </Grid>
---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        navbar: {
+            backgroundColor: 'black',
+        }
+    },
+});
+
+class NavBar extends Component {
+    render() {
+        const { classes } =  this.props;
+        return(
+            <div className="NavBar">
+                <Paper className={classes.paper}>NavBar</Paper>
+            </div>
+        );
+    }
+}
+
+NavBar.protoTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NavBar);
