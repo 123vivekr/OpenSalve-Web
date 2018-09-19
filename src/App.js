8a3731468add8892cbf8748fc8f3798abe96282d
123vivekr <123vivekr@gmail.com>
2018-09-19 22:08:57 +0530

    add request page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth from "./components/auth";
 import { apiVer } from "./utils/api.js";
+import Request from './components/request';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Router>
             <div>
                 <Route exact path="/auth" component={Auth} />
+                <Route exact path="/request" component={Request} />
             </div>
         </Router>
     );
