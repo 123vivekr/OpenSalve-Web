d75d07147c284b848e199a3446f199c1c97e9aae
Vivek R <123vivekr@gmail.com>
2018-09-24 22:14:02 +0530

    add u/username API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth from "./components/auth";
-import { apiVer, login, register } from "./utils/api.js";
+import { apiVer, login, register, userDetails } from "./utils/api.js";
 import Request from './components/request';
 import './App.css';
 
 class App extends Component {
   render() {
     console.log(apiVer());
-    console.log(login());
+      console.log(register("123vivekr", "hello", "123vivekr@gmail.com", "VIVEK"));
+      console.log(userDetails("123vivekr"));
     return (
         <Router>
             <div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,23 @@
 var axios = require('axios');
 
+const APIbase = "http://localhost:8000/api";
+
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = "csrftoken";
 
+// Get cookie by name
+// https://stackoverflow.com/questions/10730362/get-cookie-by-name
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+const csrftoken = getCookie("csrftoken");
+
 // return API version
 function apiVer() {
-    axios.get("/api")
+    axios.get(APIbase + "/")
         .then(res => {
             console.log(res.data);
             return res.data;
@@ -14,7 +26,7 @@ function apiVer() {
 
 // login via API
 function login(username, pass) {
-    axios.get("/api/accounts/login")
+    axios.get(APIbase + "/accounts/login")
         .then(res => {
             console.log(res);
             return res;
@@ -24,12 +36,13 @@ function login(username, pass) {
 // register via API
 function register(username, pass, email, name) {
     var data = {
-        username: username,
-        password: pass,
-        email: email,
-        name: name
+        "username": username,
+        "password": pass,
+        "email": email,
+        "name": name
     }
-    axios.post("http://localhost:8000/api/accounts/register", data)
+    console.log(data);
+    axios.post(APIbase + "/accounts/register", data)
         .then(res => {
             console.log(res);
         })
@@ -38,6 +51,19 @@ function register(username, pass, email, name) {
         })
 }
 
+// get user details from API
+function userDetails(username) {
+	var headers = {
+		'accept': 'application/json',
+		'X-CSRFToken': csrftoken,
+	};
+    axios.get(APIbase + "/accounts/u/" + username, {headers: headers})
+        .then(res => {
+            console.log(res);
+        })
+}
+
 module.exports.apiVer = apiVer;
 module.exports.register = register;
 module.exports.login = login;
+module.exports.userDetails = userDetails;
