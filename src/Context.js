b389fa0981c3dbd1798d36b5c70d6111520b2c9b
Aswin M Prabhu <aswinmprabhu@gmail.com>
2018-10-02 23:38:39 +0530

    added AppProvider component

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const defaultState = {
-  isLoggedIn: false,
+  isLoggedIn: false
 };
 
-const Context = React.createContext({state: defaultState});
+const Context = React.createContext({ state: defaultState });
 
 export const { Provider, Consumer } = Context;
+
+export class AppProvider extends React.Component {
+  state = {
+    isLoggedIn: false
+  };
+  toggleLoggedIn = () => {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+  };
+  render() {
+    return (
+      <Provider
+        value={{
+          state: this.state,
+          actions: {
+            toggleLoggedIn: this.toggleLoggedIn
+          }
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
