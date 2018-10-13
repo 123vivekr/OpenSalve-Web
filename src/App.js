a0c7f6231fa99ab5bca295be9913404fc923193c
Vivek R <123vivekr@gmail.com>
2018-10-13 12:39:28 +0530

    add routing to navbuttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth from "./components/auth";
+import NavBar from './components/navbar';
 import { apiVer, login, register, userDetails } from "./utils/api.js";
 import Request from './components/request';
 import { AppProvider  } from './Context'
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import { withRouter } from 'react-router';
 
 const styles = theme => ({
     paper: {
@@ -21,13 +22,13 @@ class NavBar extends Component {
         return(
             <div className="NavBar">
                 <Paper className={classes.paper}>
-                    <Button id="button" disabled={this.props.title === "request"} variant='contained' className={classes.button} color='primary'>
+                    <Button onClick={() => this.props.history.push("/request")} id="button" disabled={this.props.title === "request"} variant='contained' className={classes.button} color='primary'>
                        Request
                     </Button>
-                    <Button id="button" disabled={this.props.title === "auth"} variant='contained' className={classes.button} color='primary'>
+                    <Button onClick={() => this.props.history.push("/auth")} id="button" disabled={this.props.title === "auth"} variant='contained' className={classes.button} color='primary'>
                         Auth
                     </Button>
-                    <Button id="button" disabled={this.props.title === "help"} variant='contained' className={classes.button} color='primary'>
+                    <Button onClick={() => this.props.history.push("/help")} id="button" disabled={this.props.title === "help"} variant='contained' className={classes.button} color='primary'>
                         Help
                     </Button>
                 </Paper>
@@ -40,4 +41,4 @@ NavBar.protoTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(NavBar);
+export default withRouter(withStyles(styles)(NavBar));
