60c8dcfb15e9ef6dc361bfb86308b14167eb12b0
Aswin M Prabhu <aswinmprabhu@gmail.com>
2018-10-14 11:30:49 +0530

    add help, analytics routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth from "./components/auth";
 import { apiVer, login, register, userDetails } from "./utils/api.js";
-import Request from './components/request';
-import { AppProvider  } from './Context'
-import './App.css';
+import Request from "./components/requests";
+import Help from "./components/help";
+import Analytics from "./components/analytics";
+import { AppProvider } from "./Context";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <Router>
-            <div>
-              <AppProvider>
-                <Route exact path="/auth" component={Auth} />
-                <Route exact path="/request" component={Request} />
-              </AppProvider>
-            </div>
-        </Router>
+      <Router>
+        <div>
+          <AppProvider>
+            <Route exact path="/auth" component={Auth} />
+            <Route exact path="/requests" component={Request} />
+            <Route exact path="/analytics" component={Analytics} />
+            <Route exact path="/help" component={Help} />
+          </AppProvider>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/analytics.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import NavBar from "./navbar";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  }
+});
+
+class Analytics extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={root}>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+Analytics.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Analytics);
