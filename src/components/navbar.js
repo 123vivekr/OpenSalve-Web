f7ff321440bb57734b733e916283811f3d3dddf2
Vivek R <123vivekr@gmail.com>
2018-10-19 21:46:44 +0530

    navabar button disable feature

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,101 +9,105 @@ import { Link, withRouter } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: "center",
-    color: theme.palette.text.secondary
-  }
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: "center",
+        color: theme.palette.text.secondary
+    }
 });
 
 class NavBar extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <Consumer>
-        {context => {
-          return (
-            <div className="NavBar">
-              <Paper className={classes.paper}>
-                <Grid container spacing={0} className="navGrid">
-                  <Grid
-                    item
-                    xs={6}
-                    sm={3}
-                    styles={
-                      context.state.currentPage == "requests" && {
-                        borderBottomColor: "red",
-                        borderBottom: "2px"
-                      }
-                    }
-                  >
-                      <Button
-                        variant='contained'
-                        color='primary'
-                      >
-                    <Link
-                      to="/requests"
-                      onClick={() => context.actions.setCurrentPage("requests")}
-                      style={{textDecoration: 'none', color: 'white'}}
-                    >
-                      Requests
-                    </Link>
-                    </Button>
+    render() {
+        const { classes } = this.props;
+        return (
+            <Consumer>
+                {context => {
+                    return (
+                        <div className="NavBar">
+                            <Paper className={classes.paper}>
+                                <Grid container spacing={0} className="navGrid">
+                                    <Grid
+                                        item
+                                        xs={6}
+                                        sm={3}
+                                        styles={
+                                            context.state.currentPage == "requests" && {
+                                                borderBottomColor: "red",
+                                                borderBottom: "2px"
+                                            }
+                                        }
+                                    >
+                                        <Button
+                                            variant='contained'
+                                            color='primary'
+                                            disabled={this.props.location.pathname === '/requests'}
+                                        >
+                                            <Link
+                                                to="/requests"
+                                                onClick={() => context.actions.setCurrentPage("requests")}
+                                                style={{textDecoration: 'none', color: 'white'}}
+                                            >
+                                                Requests
+                                            </Link>
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={6} sm={3}>
+                        <Button
+                            variant='contained'
+                            color='primary'
+                            disabled={this.props.location.pathname === '/help'}
+                        >
+                            <Link
+                                to="/help"
+                                onClick={() => context.actions.setCurrentPage("help")}
+                                style={{textDecoration: 'none', color: 'white'}}
+                            >
+                                Help
+                            </Link>
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                        <Button
+                            variant='contained'
+                            color='primary'
+                            disabled={this.props.location.pathname === '/analytics'}
+                        >
+                            <Link
+                                to="/analytics"
+                                onClick={() => context.actions.setCurrentPage("analytics")}
+                                style={{textDecoration: 'none', color: 'white'}}
+                            >
+                                Anaytics
+                            </Link>
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                        <Button
+                            variant='contained'
+                            color='primary'
+                            disabled={this.props.location.pathname === '/camps'}
+                        >
+                            <Link
+                                to="/camps"
+                                onClick={() => context.actions.setCurrentPage("camps")}
+                                style={{textDecoration: 'none', color: 'white'}}
+                            >
+                                Camps
+                            </Link>
+                        </Button>
+                    </Grid>
                 </Grid>
-                  <Grid item xs={6} sm={3}>
-<Button
-                        variant='contained'
-                        color='primary'
-                      >
-                    <Link
-                      to="/help"
-                      onClick={() => context.actions.setCurrentPage("help")}
-                      style={{textDecoration: 'none', color: 'white'}}
-                    >
-Help
-                    </Link>
-                    </Button>
-                  </Grid>
-                  <Grid item xs={6} sm={3}>
-  <Button
-                        variant='contained'
-                        color='primary'
-                      >
-                    <Link
-                      to="/analytics"
-                      onClick={() => context.actions.setCurrentPage("analytics")}
-                      style={{textDecoration: 'none', color: 'white'}}
-                    >
-                        Anaytics
-                    </Link>
-                    </Button>
-                  </Grid>
-                  <Grid item xs={6} sm={3}>
-  <Button
-                        variant='contained'
-                        color='primary'
-                      >
-                    <Link
-                      to="/camps"
-                      onClick={() => context.actions.setCurrentPage("camps")}
-                      style={{textDecoration: 'none', color: 'white'}}
-                    >
-                     Camps
-                    </Link>
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </div>
-          );
-        }}
-      </Consumer>
-    );
-  }
+            </Paper>
+        </div>
+                    );
+                }}
+            </Consumer>
+        );
+    }
 }
 
 NavBar.protoTypes = {
-  classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired
 };
 
 export default withRouter(withStyles(styles)(NavBar));
