3d7ea0d128f79c8d7aa9da93abb5e0bae549b6f0
Vivek R <123vivekr@gmail.com>
2018-10-21 12:40:25 +0530

    map: add marker + google map link + styling

--- src/components/requestcard.js
+++ src/components/requestcard.js
@@ -24,9 +24,13 @@ class RequestCard extends Component {
         viewport: {
             width: 400,
             height: 200,
-            latitude: 37.7577,
-            longitude: -122.4376,
-            zoom: 8
+            latitude: 37.7756778,
+            longitude: -122.416309,
+            zoom: 15
+        },
+        marker: {
+            latitude: 37.7756778,
+            longitude: -122.416309,
         }
     };
     render() {
@@ -40,15 +44,24 @@ class RequestCard extends Component {
                         </Grid>
                         <Grid item xs={1} sm={3}>
                             Map
+                            <br />
+                            <br />
                             <ReactMapGL
                                 {...this.state.viewport}
                                 onViewportChange={(viewport) => this.setState({viewport})}
                                 mapboxApiAccessToken="pk.eyJ1IjoiYmhhc2thcnh1aSIsImEiOiJjam43ZzZkcmc2ZGJpM2txY3Y4amFwY3VwIn0.d5syOLoEU7crgzP260sjAA"
+                                style={{borderRadius: '20px'}}
                             >
-                                <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
-                                    <div style={{color: 'red'}} >x</div>
+                                <Marker latitude={this.state.marker.latitude} longitude={this.state.marker.longitude}>
+                                    <a
+                                        href={"https://www.google.com/maps/search/?api=1&query="+this.state.marker.latitude+','+this.state.marker.longitude}
+                                        style={{textDecoration: 'none', color: 'red'}}
+                                    >
+                                        x
+                                    </a>
                                 </Marker>
                             </ReactMapGL>
+                            <font size='1'>Click on the marker to open in Google Maps</font>
                         </Grid>
                     </Grid>
                 </Paper>
