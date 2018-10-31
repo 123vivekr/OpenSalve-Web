cebcf4f29fb85781b8a3a5e1cd0016d18a804209
Vivek R <123vivekr@gmail.com>
2018-10-31 22:29:44 +0530

    add details to request and card

--- src/components/helpcard.js
+++ src/components/helpcard.js
@@ -39,42 +39,64 @@ class HelpCard extends Component {
                 lat: this.props.mapData.lat,
                 lng: this.props.mapData.lng,
             }
-            });
+        });
     }
 
     render() {
         const { classes } = this.props;
+        const strike = {textDecoration: 'line-through'};
         return (
             <div className={root}>
                 <Paper className={classes.paper}>
                     <Grid container spacing={0}>
                         <Grid item xs={10} sm={3}>
                             Details
-                       </Grid>
-                        <Grid item xs={1} sm={3}>
-                            Map
-                            <br />
-                            <br />
-                            <ReactMapGL
-                                {...this.state.viewport}
-                                onViewportChange={(viewport) => this.setState({viewport})}
-                                mapboxApiAccessToken="pk.eyJ1IjoiYmhhc2thcnh1aSIsImEiOiJjam43ZzZkcmc2ZGJpM2txY3Y4amFwY3VwIn0.d5syOLoEU7crgzP260sjAA"
-                                style={{borderRadius: '20px'}}
-                            >
-                                <Marker latitude={this.state.markerReqs.lat} longitude={this.state.markerReqs.lng}>
-                                    <a
-                                        target="_blank"
-                                        href={"https://www.google.com/maps/search/?api=1&query="+this.state.markerReqs.lat+','+this.state.markerReqs.lng}
-                                        style={{textDecoration: 'none', color: 'red'}}
-                                    >
-                                        x
-                                    </a>
-                                </Marker>
-                            </ReactMapGL>
-                            <font size='1'>Click on the marker to open in Google Maps</font>
-                        </Grid>
-                    </Grid>
-                </Paper>
+            <p>
+                Name: { this.props.mapData.name }
+                <br />
+                Location: { this.props.mapData.location }
+                <br />
+                No. of people with you: { this.props.mapData.number_of_people_with_you }
+                <br />
+                Phone: { this.props.mapData.phone }
+                <br />
+                Source: { this.props.mapData.source }
+                <br />
+                Status: { this.props.mapData.status}
+                <br />
+                <section style={this.props.mapData.request_for_others ? {} : strike}>Request for others</section>
+                <br />
+                <section style={this.props.mapData.need_food ? {} : strike}>Need Food</section>
+                <section style={this.props.mapData.need_medicine ? {} : strike}>Need Medicine</section>
+                <section i style={this.props.mapData.need_rescue ? {} : strike}>Need Rescue</section>
+                <section i style={this.props.mapData.need_water ? {} : strike}>Need Water</section>
+            </p>
+
+        </Grid>
+        <Grid item xs={1} sm={3}>
+            Map
+            <br />
+            <br />
+            <ReactMapGL
+                {...this.state.viewport}
+                onViewportChange={(viewport) => this.setState({viewport})}
+                mapboxApiAccessToken="pk.eyJ1IjoiYmhhc2thcnh1aSIsImEiOiJjam43ZzZkcmc2ZGJpM2txY3Y4amFwY3VwIn0.d5syOLoEU7crgzP260sjAA"
+                style={{borderRadius: '20px'}}
+            >
+                <Marker latitude={this.state.markerReqs.lat} longitude={this.state.markerReqs.lng}>
+                    <a
+                        target="_blank"
+                        href={"https://www.google.com/maps/search/?api=1&query="+this.state.markerReqs.lat+','+this.state.markerReqs.lng}
+                        style={{textDecoration: 'none', color: 'red'}}
+                    >
+                        x
+                    </a>
+                </Marker>
+            </ReactMapGL>
+            <font size='1'>Click on the marker to open in Google Maps</font>
+        </Grid>
+    </Grid>
+</Paper>
             </div>
         );
     }
--- src/components/requestcard.js
+++ src/components/requestcard.js
@@ -44,12 +44,33 @@ class RequestCard extends Component {
 
     render() {
         const { classes } = this.props;
+        const strike = {textDecoration: 'line-through'};
         return (
             <div className={root}>
                 <Paper className={classes.paper}>
                     <Grid container spacing={0}>
                         <Grid item xs={10} sm={3}>
                             Details
+                            <p>
+                                Name: { this.props.mapData.name }
+                                <br />
+                                Location: { this.props.mapData.location }
+                                <br />
+                                No. of people with you: { this.props.mapData.number_of_people_with_you }
+                                <br />
+                                Phone: { this.props.mapData.phone }
+                                <br />
+                                Source: { this.props.mapData.source }
+                                <br />
+                                Status: { this.props.mapData.status}
+                                <br />
+                                <section style={this.props.mapData.request_for_others ? {} : strike}>Request for others</section>
+                                <br />
+                                <section style={this.props.mapData.need_food ? {} : strike}>Need Food</section>
+                                <section style={this.props.mapData.need_medicine ? {} : strike}>Need Medicine</section>
+                                <section i style={this.props.mapData.need_rescue ? {} : strike}>Need Rescue</section>
+                                <section i style={this.props.mapData.need_water ? {} : strike}>Need Water</section>
+                            </p>
                        </Grid>
                         <Grid item xs={1} sm={3}>
                             Map
